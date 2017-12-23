e543d7a0004addb3156690a98ed9cf98b304469b
Adrian Kumpf <adrian@peaksandpies.com>
2017-12-23 17:55:47 +0100

    Generate docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule TelegramLoggerBackend.Mixfile do
       version: "0.3.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+
+      # Docs
+      name: "TelegramLoggerBackend",
+      description: " A logger backend for posting messages to Telegram.",
+      source_url: "https://github.com/adriankumpf/telegram-logger-backend",
+      docs: [main: "readme", extras: ["README.md"]]
     ]
   end
 
@@ -20,10 +27,19 @@ defmodule TelegramLoggerBackend.Mixfile do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:exvcr, "~> 0.8", only: :test},
       {:gen_stage, "~> 0.12"},
       {:httpoison, "~> 0.13"},
-      {:poison, "~> 3.1"},
-      {:exvcr, "~> 0.8", only: :test}
+      {:poison, "~> 3.1"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/adriankumpf/telegram-logger-backend"},
+      maintainers: ["Adrian Kumpf"]
     ]
   end
 end
